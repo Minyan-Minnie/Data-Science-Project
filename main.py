8175301516b2c75838506966b18c4ec87d6669fe
Minyan-Minnie <mj734@rutgers.edu>
2026-05-03 02:58:13 -0400

    initializing stuff in main

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-fjjsdkfsdjkjfskljfklsjfkl
+import pandas as pd
+
+df = pd.read_csv("dataset.csv")
+
+feats = [ "danceability","energy","loudness","tempo","valence","instrumentalness","speechiness"]
+pred_popularity = "popularity"
+
+df = df[feats + [pred_popularity]]      #gets us only the cols that we want
+df = df.dropna()
+
+df["popular"] = (df["popularity"] >= 70).astype(int)
+y = df[feats]
+y = df["popular"]
+
+
+print(df.head())
+print(df.columns)
